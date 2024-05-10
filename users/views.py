eefdf886e9a149cc053b057082a80f6a32fd2669
MehediHasanNasim <75909031+MehediHasanNasim@users.noreply.github.com>
2024-05-10 23:21:54 +0600

    first commit

---
+++ users/views.py
@@ -0,0 +1,44 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from .forms import LoginForm
+from django.contrib.auth.decorators import login_required 
+
+def user_login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            user = authenticate(request, username=data['username'], password=data['password'])
+            if user is not None:
+                login(request, user)
+                return HttpResponse("user authenticated and logged in")
+            else:
+                return HttpResponse('Invalid credentials')
+    else:
+        form = LoginForm()
+    return render(request, 'users/login.html', {'form': form})
+
+
+
+# @login_required
+def index(request):
+    return render(request, 'users/index.html')
+# def user_login(request):
+#     if request.method == 'POST':
+#         form = LoginForm(request.POST)
+#         if form.is_valid():
+#             username = request.POST.get('username')
+#             password = request.POST.get('password')
+#             user = authenticate(request, username=username, password=password)
+
+#             if user is not None:
+#                 login(request, user)
+#                 return HttpResponse("user authenticated and logged in")
+            
+#             else:
+#                 return HttpResponse('Invalid credentials')
+#     else:
+#         form = LoginForm()
+
+#     return render(request, 'users/login.html', {'form': form})
