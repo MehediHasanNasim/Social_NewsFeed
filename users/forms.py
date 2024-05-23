66e5f4149e9c1e8f092dc24586c5c5082f53f5d0
MehediHasanNasim <75909031+MehediHasanNasim@users.noreply.github.com>
2024-05-24 02:48:37 +0600

    Creating register forms

--- users/forms.py
+++ users/forms.py
@@ -4,3 +4,21 @@ class LoginForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
     
+    
+class UserRegistrationForm(forms.ModelForm):
+    
+    password = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Confirm Password', widget=forms.PasswordInput)
+    
+    
+    class Meta:
+        model = 
+        fields = {'username', 'email', 'first_name'}
+    
+    def check_password(self):
+        if self.cleaned_data["password"] != self.clean_data['password2']:
+            raise forms.ValidationError("Passwords aren't matching")
+        return self.clean_data['password2']
+        
+
+
