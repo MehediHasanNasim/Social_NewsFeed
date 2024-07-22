e09078a184ae068321c7971482a57f72767c44e7
MehediHasanNasim <75909031+MehediHasanNasim@users.noreply.github.com>
2024-07-22 19:11:36 +0600

    creating index page for showing post

--- users/views.py
+++ users/views.py
@@ -5,6 +5,8 @@ from .forms import LoginForm, UserRegistrationForm
 from django.contrib.auth.decorators import login_required 
 from .models import *
 from .forms import * 
+from posts.models import Post
+
 
 def user_login(request):
     if request.method == 'POST':
@@ -25,7 +27,9 @@ def user_login(request):
 
 @login_required
 def index(request):
-    return render(request, 'users/index.html')
+    current_user = request.user
+    posts = Post.objects.filter(user=current_user)
+    return render(request, 'users/index.html', {'posts': posts})
 # def user_login(request):
 #     if request.method == 'POST':
 #         form = LoginForm(request.POST)
