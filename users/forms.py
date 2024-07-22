aed3881ed4483f7e7cd7dfe333d027800e9e735b
MehediHasanNasim <75909031+MehediHasanNasim@users.noreply.github.com>
2024-07-22 18:16:24 +0600

    creating post app & its models

--- users/forms.py
+++ users/forms.py
@@ -2,7 +2,17 @@ from django import forms
 from django.contrib.auth.models import User
 from .models import Profile
 
-# class UserEditForm(forms)
+class UserEditForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'email')
+        
+        
+class ProfileEditForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ('photo',)
+        
 
 class LoginForm(forms.Form):
     username = forms.CharField()
@@ -24,5 +34,3 @@ class UserRegistrationForm(forms.ModelForm):
             raise forms.ValidationError("Passwords aren't matching")
         return self.clean_data['password2']
         
-
-
